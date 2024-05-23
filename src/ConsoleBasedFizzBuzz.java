ac7c57b7f4bb2218e9c04619d13f1044f9a89b83
Santiago Gallegos Juan Alejandro <zS22017011@estudiantes.uv.mx>
2024-05-23 14:52:14 -0600

    X

--- src/ConsoleBasedFizzBuzz.java
+++ src/ConsoleBasedFizzBuzz.java
@@ -2,9 +2,13 @@ class ConsoleBasedFizzBuzz implements FizzBuzz{
     @Override
     public void print(int from, int to) {
         for (int i = from; i <= to; i++) {
-            if (i % 3 == 0 && i % 5 == 0) System.out.println("FizzBuzz");
-            if (i % 3 == 0) System.out.println("Fizz");
-            if (i % 5 == 0) System.out.println("Buzz");
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+                System.out.println("Fizz");
+            else if (i % 5 == 0)
+                System.out.println("Buzz");
             if (i % 3 != 0 && i % 5 != 0) System.out.println(i);
         }
     }
